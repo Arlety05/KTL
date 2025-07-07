61135be41b9ddb45ddf77d25c73aeddb0d09bdac
Arlety Mejia <amejiarosario81@gmail.com>
2025-07-07 17:43:33 -0400

    Add files via upload

---
+++ index_resources/js/ardoraSopaCFG.js
@@ -0,0 +1,35 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var successes=0; successesMax=1; attempts=1; attemptsMax=1;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var startTime; tiSuccesses=false; tiAttempts=false; tiScore=false;
+typeGame=0;
+var colorBack="#C0C0C0"; colorButton="#91962F"; colorText="#000000"; colorSele="#251BE4";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=1; scoWrong=0; scoOkDo=1; scoWrongDo=1; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Felicidades a logrado encotrar todas las palabras, untuacion  10/10 "; messageTime="Intentelo de nuevo"; isShowMessage=false;
+var urlOk=""; urlTime="";
+var goURLOk="_blank"; goURLTime="_blank"; 
+var borderOk="#008000"; borderTime="#FF0000"; borderAttempts="#FF0000";borderError="#FF0000";
+var imaSel=0; texSel=0;
+var board=[]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=["Q0FNSU9O","Q0FNSVNB","UlVFREE=","TU9UT1I="];
+var c=[6,6,5,5];
+var answers=[]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="aW5kZXg="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var numCols=4; numRows=4; sizeCell=20;
+var direction=["HD","HI","VD","VI"];
+var boardGame=[[".",".",".","."],[".",".",".","."],[".",".",".","."],[".",".",".","."]];
+var solved1X=[];solved1Y=[];solved2X=[];solved2Y=[];paintSolved=["N","N","N","N"];x1=[];y1=[];x2=[];y2=[];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
